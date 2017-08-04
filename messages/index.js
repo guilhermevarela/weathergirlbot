303bb0a9c317882494666170b26f7b5a3bba7fff
Guilherme Varela <guilhermevarela@hotmail.com>
2017-08-04 16:16:41 -0300

    greeting intent update (2)

--- messages/index.js
+++ messages/index.js
@@ -35,7 +35,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
-.matches('Make-appointment', (session, args) => {
+.matches('greeting', (session, args) => {
     session.send('Hello you, what\'s up?');
 })
 .onDefault((session) => {
